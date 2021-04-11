5fac44d5bfae9c985a687a13ef7d854204fda0e2
adheias <adheiasramadhan@gmail.com>
2021-04-11 23:19:06 +0700

    Initial Commit

---
+++ app/src/main/java/com/dicoding/picodiploma/mysettingpreference/MyPreferenceFragment.kt
@@ -0,0 +1,43 @@
+package com.dicoding.picodiploma.mysettingpreference
+
+import android.os.Bundle
+import androidx.preference.CheckBoxPreference
+import androidx.preference.EditTextPreference
+import androidx.preference.PreferenceFragmentCompat
+
+class MyPreferenceFragment : PreferenceFragmentCompat() {
+    private lateinit var NAME: String
+    private lateinit var EMAIL: String
+    private lateinit var AGE: String
+    private lateinit var PHONE: String
+    private lateinit var LOVE: String
+
+    private lateinit var namePreference: EditTextPreference
+    private lateinit var emailPreference: EditTextPreference
+    private lateinit var agePreference: EditTextPreference
+    private lateinit var phonePreference: EditTextPreference
+    private lateinit var isLoveMUPreference: CheckBoxPreference
+
+    companion object {
+        private const val DEFAULT_VALUE = "Tidak ada"
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        addPreferencesFromResource(R.xml.preferences)
+        init()
+    }
+
+    private fun init() {
+        NAME = resources.getString(R.string.key_name)
+        EMAIL = resources.getString(R.string.key_email)
+        AGE = resources.getString(R.string.key_age)
+        PHONE = resources.getString(R.string.key_phone)
+        LOVE = resources.getString(R.string.key_love)
+
+        namePreference = findPreference<EditTextPreference>(NAME) as EditTextPreference
+        emailPreference = findPreference<EditTextPreference>(EMAIL) as EditTextPreference
+        agePreference = findPreference<EditTextPreference>(AGE) as EditTextPreference
+        phonePreference = findPreference<EditTextPreference>(PHONE) as EditTextPreference
+        isLoveMUPreference = findPreference<CheckBoxPreference>(LOVE) as EditTextPreference
+    }
+}
