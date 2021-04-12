349af2f3ed4a62925b8b5076799b5642850d80b5
adheias <adheiasramadhan@gmail.com>
2021-04-12 07:37:21 +0700

    Initial Commit

--- app/src/main/java/com/dicoding/picodiploma/mysettingpreference/MyPreferenceFragment.kt
+++ app/src/main/java/com/dicoding/picodiploma/mysettingpreference/MyPreferenceFragment.kt
@@ -1,11 +1,12 @@
 package com.dicoding.picodiploma.mysettingpreference
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.preference.CheckBoxPreference
 import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
 
-class MyPreferenceFragment : PreferenceFragmentCompat() {
+class MyPreferenceFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
     private lateinit var NAME: String
     private lateinit var EMAIL: String
     private lateinit var AGE: String
@@ -25,6 +26,16 @@ class MyPreferenceFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.preferences)
         init()
+        setSummaries()
+    }
+
+    private fun setSummaries() {
+        val sh = preferenceManager.sharedPreferences
+        namePreference.summary = sh.getString(NAME, DEFAULT_VALUE)
+        emailPreference.summary = sh.getString(EMAIL, DEFAULT_VALUE)
+        agePreference.summary = sh.getString(AGE, DEFAULT_VALUE)
+        phonePreference.summary = sh.getString(PHONE, DEFAULT_VALUE)
+        isLoveMUPreference.isChecked = sh.getBoolean(LOVE, false)
     }
 
     private fun init() {
@@ -39,5 +50,37 @@ class MyPreferenceFragment : PreferenceFragmentCompat() {
         agePreference = findPreference<EditTextPreference>(AGE) as EditTextPreference
         phonePreference = findPreference<EditTextPreference>(PHONE) as EditTextPreference
         isLoveMUPreference = findPreference<CheckBoxPreference>(LOVE) as CheckBoxPreference
+    }
+
+    override fun onResume() {
+        super.onResume()
+        preferenceScreen.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        preferenceScreen.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
+        if (key == NAME) {
+            namePreference.summary = sharedPreferences.getString(NAME, DEFAULT_VALUE)
+        }
+
+        if (key == EMAIL) {
+            emailPreference.summary = sharedPreferences.getString(EMAIL, DEFAULT_VALUE)
+        }
+
+        if (key == AGE) {
+            agePreference.summary = sharedPreferences.getString(AGE, DEFAULT_VALUE)
+        }
+
+        if (key == PHONE) {
+            phonePreference.summary = sharedPreferences.getString(PHONE, DEFAULT_VALUE)
+        }
+
+        if (key == LOVE) {
+            isLoveMUPreference.isChecked = sharedPreferences.getBoolean(LOVE, false)
+        }
     }
 }
